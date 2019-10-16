2f4f09bc11db95c05edc71260e880687305a0567
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-16 11:03:41 +0200

    chore: Remove tutorial comments

--- admin/class-appq-integration-center-admin.php
+++ admin/class-appq-integration-center-admin.php
@@ -13,8 +13,6 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
  *
  * @package    AppQ_Integration_Center
  * @subpackage AppQ_Integration_Center/admin
@@ -61,18 +59,6 @@ class AppQ_Integration_Center_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in AppQ_Integration_Center_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The AppQ_Integration_Center_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->appq_integration_center, plugin_dir_url( __FILE__ ) . 'css/appq-integration-center-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -83,18 +69,6 @@ class AppQ_Integration_Center_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in AppQ_Integration_Center_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The AppQ_Integration_Center_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_script( $this->appq_integration_center, plugin_dir_url( __FILE__ ) . 'js/appq-integration-center-admin.js', array( 'jquery' ), $this->version, false );
 
