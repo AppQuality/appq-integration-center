da4fe0917db5304df361c5308d95706f4d56d731
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-18 15:05:00 +0200

    feat: Keep current tab open

--- admin/js/appq-integration-center-admin.js
+++ admin/js/appq-integration-center-admin.js
@@ -9,7 +9,12 @@
 			tabNav.find('.nav-link').removeClass('active')
 			$(this).addClass('active')
 		})
-
+		$('#campaign-tabs .nav-link').click(function(){
+			window.location.hash = $(this).attr('href')
+		})
+		
+		var hash = window.location.hash;
+		$('#campaign-tabs a[href="' + hash + '"]').click();
 
 	});
 
