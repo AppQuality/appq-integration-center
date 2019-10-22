1bc9a93ad24274e933728a3da3758875ff7484b3
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-22 09:48:46 +0200

    fix: Remove superflous js for tabs

--- admin/js/appq-integration-center-admin.js
+++ admin/js/appq-integration-center-admin.js
@@ -2,13 +2,6 @@
 	'use strict';
 	
 	$( document ).ready(function() {
-		
-		$('.nav-link').click(function(){
-			var tabNav = $(this).closest('.nav')
-			
-			tabNav.find('.nav-link').removeClass('active')
-			$(this).addClass('active')
-		})
 		$('#campaign-tabs .nav-link').click(function(){
 			window.location.hash = $(this).attr('href')
 		})
