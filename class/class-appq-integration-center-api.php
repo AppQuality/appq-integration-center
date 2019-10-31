fd261b828cefec8e77312612a04bfef455b015ac
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-31 13:04:03 +0100

    feat: Add media_links {placeholder} support

--- class/class-appq-integration-center-api.php
+++ class/class-appq-integration-center-api.php
@@ -224,10 +224,11 @@ class IntegrationCenterRestApi
 		$value = str_replace('{Bug.os_version}', $bug->os_version, $value);
 		
 		// Only if {Bug.media} exists
-		if (strpos($value,'{Bug.media}') !== false)
+		if (strpos($value,'{Bug.media}') !== false || strpos($value,'{Bug.media_links}') !== false )
 		{
 			$media =  $wpdb->get_col($wpdb->prepare('SELECT location FROM ' . $wpdb->prefix . 'appq_evd_bug_media WHERE bug_id = %d', $bug->id)); 
 			$value = str_replace('{Bug.media}', implode(', ',$media), $value);
+			$value = str_replace('{Bug.media_links}', implode(', ',$media), $value);
 		}
 		
 		if (property_exists($bug, 'fields') && sizeof($bug->fields) > 0)
