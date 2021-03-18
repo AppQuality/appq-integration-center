d533500a564453e011c1a60d5c9a588be5cf32be
Davide Bizzi <davide.bizzi@app-quality.com>
2021-03-18 12:57:14 +0100

    feat: Add card to nav tabs

--- admin/partials/appq-integration-center-admin-bugs/list.php
+++ admin/partials/appq-integration-center-admin-bugs/list.php
@@ -10,75 +10,77 @@
  */
 
 ?>
-<br>
-<div class="row">
-    <div class="col d-flex flex-column">
-        <div class="input-group mb-3">
-            <input id="bugs_list-search" class="form-control custom-search" type="search" placeholder="Search by id, name, category, status, severity,tags or 'duplicated','unique','uploaded','to-upload'. The search is in OR, use + to search in AND (e.g. HIGH+approved)" aria-label="Search">
+<div class="card">
+  <div class="card-body">
+    <div class="row">
+        <div class="col d-flex flex-column">
+            <div class="input-group mb-3">
+                <input id="bugs_list-search" class="form-control custom-search" type="search" placeholder="Search by id, name, category, status, severity,tags or 'duplicated','unique','uploaded','to-upload'. The search is in OR, use + to search in AND (e.g. HIGH+approved)" aria-label="Search">
+            </div>
         </div>
-    </div>
 
-    <div class="col-sm-auto">
-        <div class="btn-group" role="group" aria-label="table controls button group">
-            <button class="send-all btn btn-primary btn">Send All</button>
-            <button class="send-selected btn btn-primary btn">Send Selected</button>
+        <div class="col-sm-auto">
+            <div class="btn-group" role="group" aria-label="table controls button group">
+                <button class="send-all btn btn-primary btn">Send All</button>
+                <button class="send-selected btn btn-primary btn">Send Selected</button>
+            </div>
         </div>
     </div>
-</div>
-<table class="table table-striped">
-    <thead>
-    <tr>
-        <th><input class="select_all" type="checkbox" aria-label="Select all for upload"></th>
-        <th>#</th>
-        <th>Message</th>
-        <th>Category</th>
-        <th>Status</th>
-        <th>Severity</th>
-        <th>Duplicated</th>
-        <th>Tags</th>
-        <th></th>
-        <th></th>
-    </tr>
-    </thead>
-    <tbody class="list">
-	<?php foreach ($bugs as $bug) : ?>
+    <table class="table table-striped">
+        <thead>
         <tr>
-            <td class="uploaded" style="display:none"><?= $bug->uploaded ? 'uploaded' : 'to-upload' ?></td>
-            <td class="duplicated" style="display:none"><?= $bug->is_duplicated ? 'duplicated' : 'unique' ?></td>
-            <td>
-                <input class="check" type="checkbox" aria-label="Select for upload">
-            </td>
-            <td class="id"><?= $bug->id ?></td>
-            <td class="name"><?= $bug->message ?></td>
-            <td class="category"><?= $bug->category ?></td>
-            <td class="status"><?= $bug->status ?></td>
-            <td class="severity"><?= $bug->severity ?></td>
-            <td><?= $bug->is_duplicated ? '<span class="fa fa-check"></span>' : '' ?></td>
-            <td class="tags"><?= '#' . implode(', #',$bug->tags) ?></td>
-            <td style="display:flex">
-				<?php if ($bug->uploaded) : ?>
-					<?php if (property_exists($bug,'bugtracker_id')) : ?>
-                        <button data-bug-id="<?= $bug->id ?>" class="btn-link text-dark btn fa fa-upload update_bug"></button>
-                        <button data-bug-id="<?= $bug->id ?>" data-bugtracker-id="<?= $bug->bugtracker_id ?>" class="btn delete_issue fa fa-close text-danger"></button>
-					<?php else : ?>
-                        <span class="fa fa-upload text-secondary btn-flat btn"></span>
-					<?php endif ?>
-				<?php else: ?>
-                    <button data-bug-id="<?= $bug->id ?>" class="btn-link text-dark btn fa fa-upload upload_bug"></button>
-				<?php endif ?>
-            <td class="is_uploaded">
-				<?php if ($bug->uploaded): ?>
-					<?php if ($bug->bugtracker_url) : ?>
-                        <a href="<?=$bug->bugtracker_url ?>" target="_blank"><span class="fa fa-external-link"></span></a>
-					<?php else: ?>
-                        <span class="fa fa-check"></span>
-					<?php endif ?>
-				<?php endif ?>
-            </td>
+            <th><input class="select_all" type="checkbox" aria-label="Select all for upload"></th>
+            <th>#</th>
+            <th>Message</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Severity</th>
+            <th>Duplicated</th>
+            <th>Tags</th>
+            <th></th>
+            <th></th>
         </tr>
-	<?php endforeach ?>
-    </tbody>
-</table>
-<button class="send-all btn-primary btn">Send All</button>
-<button class="send-selected btn-primary btn">Send Selected</button>
-
+        </thead>
+        <tbody class="list">
+    	<?php foreach ($bugs as $bug) : ?>
+            <tr>
+                <td class="uploaded" style="display:none"><?= $bug->uploaded ? 'uploaded' : 'to-upload' ?></td>
+                <td class="duplicated" style="display:none"><?= $bug->is_duplicated ? 'duplicated' : 'unique' ?></td>
+                <td>
+                    <input class="check" type="checkbox" aria-label="Select for upload">
+                </td>
+                <td class="id"><?= $bug->id ?></td>
+                <td class="name"><?= $bug->message ?></td>
+                <td class="category"><?= $bug->category ?></td>
+                <td class="status"><?= $bug->status ?></td>
+                <td class="severity"><?= $bug->severity ?></td>
+                <td><?= $bug->is_duplicated ? '<span class="fa fa-check"></span>' : '' ?></td>
+                <td class="tags"><?= '#' . implode(', #',$bug->tags) ?></td>
+                <td style="display:flex">
+    				<?php if ($bug->uploaded) : ?>
+    					<?php if (property_exists($bug,'bugtracker_id')) : ?>
+                            <button data-bug-id="<?= $bug->id ?>" class="btn-link text-dark btn fa fa-upload update_bug"></button>
+                            <button data-bug-id="<?= $bug->id ?>" data-bugtracker-id="<?= $bug->bugtracker_id ?>" class="btn delete_issue fa fa-close text-danger"></button>
+    					<?php else : ?>
+                            <span class="fa fa-upload text-secondary btn-flat btn"></span>
+    					<?php endif ?>
+    				<?php else: ?>
+                        <button data-bug-id="<?= $bug->id ?>" class="btn-link text-dark btn fa fa-upload upload_bug"></button>
+    				<?php endif ?>
+                <td class="is_uploaded">
+    				<?php if ($bug->uploaded): ?>
+    					<?php if ($bug->bugtracker_url) : ?>
+                            <a href="<?=$bug->bugtracker_url ?>" target="_blank"><span class="fa fa-external-link"></span></a>
+    					<?php else: ?>
+                            <span class="fa fa-check"></span>
+    					<?php endif ?>
+    				<?php endif ?>
+                </td>
+            </tr>
+    	<?php endforeach ?>
+        </tbody>
+    </table>
+    <button class="send-all btn-primary btn">Send All</button>
+    <button class="send-selected btn-primary btn">Send Selected</button>
+  </div>
+</div>
--- admin/partials/appq-integration-center-admin-bugs/settings.php
+++ admin/partials/appq-integration-center-admin-bugs/settings.php
@@ -9,99 +9,101 @@
  * @Last modified time: 25/10/2019
  */
 ?>
-<div class="py-4">
-	<?php if ( empty( $campaign->bugtracker ) ): ?>
-        <div class="settings-wizard">
-            <div class="info pb-5">
-				<?php
-				printf( '<h3 class="title">%s</h3>', __( 'Setup your issue tracker', $this->plugin_name ) );
-				printf( '<p class="description">%s</p>', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', $this->plugin_name ) );
-				?>
-            </div>
-            <div class="actions">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-6 col-lg-4 offset-lg-2 text-center">
-                            <div class="wrapper p-3">
-								<?php
-								printf( '<h4>%s</h4>', __( 'Import settings from an existing campaign', $this->plugin_name ) );
-								printf( '<p>%s</p>', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', $this->plugin_name ) );
-								printf( '<button data-toggle="modal" data-target="#import_tracker_settings_modal" type="button" class="btn btn-primary mt-3">%s</button>', __( 'Import settings', $this->plugin_name ) );
-								?>
-                            </div>
-                        </div>
-                        <div class="col-6 col-lg-4 text-center">
-                            <div class="wrapper p-3">
-								<?php
-								printf( '<h4>%s</h4>', __( 'Create a new setup', $this->plugin_name ) );
-								printf( '<p>%s</p>', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', $this->plugin_name ) );
-								printf( '<button data-toggle="modal" data-target="#custom_tracker_settings_modal" type="button" class="btn btn-secondary mt-3">%s</button>', __( 'Setup manually', $this->plugin_name ) );
-								?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-	<?php else :
-	$bugtracker_slug = $campaign->bugtracker->integration;
-	$bugtracker = array_filter( $integrations, function ( $var ) use ( $bugtracker_slug ) {
-		return ( $var['slug'] == $bugtracker_slug );
-	} );
-	sort( $bugtracker );
-	$bugtracker = $bugtracker[0];
-	$class      = $bugtracker['class'];
+<div class="card">
+	<div class="py-4 card-body">
+		<?php if ( empty( $campaign->bugtracker ) ): ?>
+	        <div class="settings-wizard">
+	            <div class="info pb-5">
+					<?php
+					printf( '<h3 class="title">%s</h3>', __( 'Setup your issue tracker', $this->plugin_name ) );
+					printf( '<p class="description">%s</p>', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', $this->plugin_name ) );
+					?>
+	            </div>
+	            <div class="actions">
+	                <div class="container">
+	                    <div class="row">
+	                        <div class="col-6 col-lg-4 offset-lg-2 text-center">
+	                            <div class="wrapper p-3">
+									<?php
+									printf( '<h4>%s</h4>', __( 'Import settings from an existing campaign', $this->plugin_name ) );
+									printf( '<p>%s</p>', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', $this->plugin_name ) );
+									printf( '<button data-toggle="modal" data-target="#import_tracker_settings_modal" type="button" class="btn btn-primary mt-3">%s</button>', __( 'Import settings', $this->plugin_name ) );
+									?>
+	                            </div>
+	                        </div>
+	                        <div class="col-6 col-lg-4 text-center">
+	                            <div class="wrapper p-3">
+									<?php
+									printf( '<h4>%s</h4>', __( 'Create a new setup', $this->plugin_name ) );
+									printf( '<p>%s</p>', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', $this->plugin_name ) );
+									printf( '<button data-toggle="modal" data-target="#custom_tracker_settings_modal" type="button" class="btn btn-secondary mt-3">%s</button>', __( 'Setup manually', $this->plugin_name ) );
+									?>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+		<?php else :
+		$bugtracker_slug = $campaign->bugtracker->integration;
+		$bugtracker = array_filter( $integrations, function ( $var ) use ( $bugtracker_slug ) {
+			return ( $var['slug'] == $bugtracker_slug );
+		} );
+		sort( $bugtracker );
+		$bugtracker = $bugtracker[0];
+		$class      = $bugtracker['class'];
+		?>
+	    <div class="settings">
+	        <div class="settings-group mb-3">
+				<?php $class->current_setup( $campaign ) ?>
+	        </div>
+	        <div class="settings-group available_fields">
+	            <div class="accordion" id="accordionFields">
+	                <div class="row">
+	                    <div class="col-12">
+	                        <a class="btn btn-link btn-block p-0" type="button" data-toggle="collapse" data-target="#available_fields" aria-expanded="true" aria-controls="available_fields">
+	                            <div class="card-header">
+	                                <h5 class="m-0 text-left">
+										<?= __( 'Available fields', $this->plugin_name ); ?>
+	                                    <i class="fa fa-angle-down"></i>
+	                                </h5>
+	                            </div>
+	                        </a>
+	                    </div>
+	                </div>
+
+	                <div id="available_fields" class="collapse" aria-labelledby="available_fields" data-parent="#accordionFields">
+	                    <div class="row">
+	                        <div class="col">
+	                            <button type="button" class="btn btn-link float-right" data-toggle="modal" data-target="#add_field_modal">
+			                        <?php _e( 'Add new field', $this->plugin_name ); ?>
+	                            </button>
+	                        </div>
+	                    </div>
+
+						<?php $this->available_fields( $campaign ) ?>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+
+	    <!-- START .fields_mapping -->
+	    <div class="settings-group fields_mapping border-0">
+			<?php
+			$class->get_settings( $campaign, 'fields-settings' ) ?>
+	    </div><!-- END .fields_mapping -->
+	</div>
+	<?php endif;
+
+	$this->partial( 'bugs/modal/new-available-field' );
+	$this->partial( 'bugs/modal/import-tracker', [
+		'campaigns' => AppQ_Integration_Center_Admin::get_campaigns()
+	] );
+	$this->partial( 'bugs/modal/edit-tracker', [
+		'campaign'     => $campaign,
+		'integrations' => $integrations,
+	] );
+	$this->partial( 'bugs/modal/delete-tracker-settings' );
 	?>
-    <div class="settings">
-        <div class="settings-group mb-3">
-			<?php $class->current_setup( $campaign ) ?>
-        </div>
-        <div class="settings-group available_fields">
-            <div class="accordion" id="accordionFields">
-                <div class="row">
-                    <div class="col-12">
-                        <a class="btn btn-link btn-block p-0" type="button" data-toggle="collapse" data-target="#available_fields" aria-expanded="true" aria-controls="available_fields">
-                            <div class="card-header">
-                                <h5 class="m-0 text-left">
-									<?= __( 'Available fields', $this->plugin_name ); ?>
-                                    <i class="fa fa-angle-down"></i>
-                                </h5>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div id="available_fields" class="collapse" aria-labelledby="available_fields" data-parent="#accordionFields">
-                    <div class="row">
-                        <div class="col">
-                            <button type="button" class="btn btn-link float-right" data-toggle="modal" data-target="#add_field_modal">
-		                        <?php _e( 'Add new field', $this->plugin_name ); ?>
-                            </button>
-                        </div>
-                    </div>
-
-					<?php $this->available_fields( $campaign ) ?>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- START .fields_mapping -->
-    <div class="settings-group fields_mapping border-0">
-		<?php
-		$class->get_settings( $campaign, 'fields-settings' ) ?>
-    </div><!-- END .fields_mapping -->
-</div>
-<?php endif;
-
-$this->partial( 'bugs/modal/new-available-field' );
-$this->partial( 'bugs/modal/import-tracker', [
-	'campaigns' => AppQ_Integration_Center_Admin::get_campaigns()
-] );
-$this->partial( 'bugs/modal/edit-tracker', [
-	'campaign'     => $campaign,
-	'integrations' => $integrations,
-] );
-$this->partial( 'bugs/modal/delete-tracker-settings' );
-?>
+	</div>
 </div>
