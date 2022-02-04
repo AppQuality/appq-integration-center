696f8bd5d0aa9d45300e36899291c0505ca8e16d
Luca Cannarozzo <cannarozzoluca@live.it>
2022-02-04 15:27:06 +0100

    update select2

--- src/admin/scripts/components/select2.js
+++ src/admin/scripts/components/select2.js
@@ -4,7 +4,8 @@
     $.fn.modal.Constructor.prototype.enforceFocus = function () {};
     $(".ux-select").each(function(){
       var args = {
-        width: "100%"
+        width: "100%",
+        minimumResultsForSearch: 5
       }
       if ($(this).data('parent') && $($(this).data('parent')).length) {
         args.dropdownParent = $($(this).data('parent'));
